dc2f3c4ca708203f8ef128d93d8a9decc4740113
yuan <13742342+yuantestworkspace@user.noreply.gitee.com>
2026-03-03 12:55:00 +0800

    修改日志，验证流程

--- src/handlers/llm/minicpm/llm_handler_minicpm.py
+++ src/handlers/llm/minicpm/llm_handler_minicpm.py
@@ -308,7 +308,7 @@ class HandlerS2SMiniCPM(HandlerBase, ABC):
 
         speech_end = inputs.data.get_meta("human_speech_end", False)
         logger.error(f"Put video frame, count={context.video_frame_count}")
-        if not speech_end and context.video_frame_count < 1147:
+        if not speech_end and context.video_frame_count < 20:
             logger.error(f"Handling session={str(context.local_session_id)}, inputs={inputs}")
             return
 
