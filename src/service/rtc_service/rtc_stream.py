2804d20114ade943cafc2b66d98f02d07ff006bc
yuan <13742342+yuantestworkspace@user.noreply.gitee.com>
2026-03-09 17:32:35 +0800

    尝试添加视频的代码

--- src/service/rtc_service/rtc_stream.py
+++ src/service/rtc_service/rtc_stream.py
@@ -121,7 +121,6 @@ class RtcStream(AsyncAudioVideoStreamHandler):
                     continue
                 sample_num = audio_array.shape[-1]
                 self.emit_counter.add_property("audio_emit", sample_num / self.output_sample_rate)
-                logger.error(f"self.output_sample_rate={self.output_sample_rate}, audio_array={audio_array}")
                 return self.output_sample_rate, audio_array
         except Exception as e:
             logger.opt(exception=e).error("Error in emit: ")
@@ -129,6 +128,7 @@ class RtcStream(AsyncAudioVideoStreamHandler):
 
     async def video_emit(self) -> VideoEmitType:
         try:
+            logger.error("video_emit")
             if not self.first_audio_emitted:
                 await asyncio.sleep(0.1)
             
